05ae8b85ad8f287f362cb6c2548174c93d4d62e2
Daniel Gallichan <dgallichan@yahoo.com>
2021-06-08 20:42:16 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 radio.onReceivedNumber(function (receivedNumber) {
     led.toggle(4, 4)
-    serial.writeValue("compass", receivedNumber)
+})
+radio.onReceivedString(function (receivedString) {
+    led.toggle(0, 0)
+    serial.writeString(receivedString)
+})
+radio.onReceivedValue(function (name, value) {
+    led.toggle(3, 3)
+    serial.writeValue(name, value)
 })
 radio.setGroup(99)
 basic.forever(function () {
