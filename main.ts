10b858a6f58019df02994a4d87cebe78cdf5042c
Daniel Gallichan <dgallichan@yahoo.com>
2021-10-12 12:55:47 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,23 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    led.toggle(4, 4)
+    led.toggle(1, 4)
     serial.writeNumber(receivedNumber)
 })
 radio.onReceivedString(function (receivedString) {
-    led.toggle(0, 0)
+    led.toggle(4, 4)
     serial.writeString(receivedString)
 })
 radio.onReceivedValue(function (name, value) {
-    led.toggle(3, 3)
+    led.toggle(4, 1)
     serial.writeValue(name, value)
 })
 radio.setGroup(99)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # # # . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
 	
 })
